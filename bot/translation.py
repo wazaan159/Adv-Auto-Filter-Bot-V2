a24ac93f1b6d445ceaa2b02518d8391506fa85c7
wazaan159 <84114353+wazaan159@users.noreply.github.com>
2021-05-13 11:54:27 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) @AlbertEinsteinTG
+# (c) @Cinema_FestivalTG
 
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+<i>I am Just a Advance Auto Search Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+Don't Waste your Time. I am Reserved for Cinema Festival Channel 🔥🔥😝
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@Cinema_FestivalTG
 </i>"""    
     
     HELP_TEXT = """
@@ -43,16 +43,16 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@Cinema_FestivalTG
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Cinema_FestivalTG">Cinema Festival</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Group</b> : <i><a href="https://t.me/CF_OfficialGroup">Join Now</a></i>
 """
